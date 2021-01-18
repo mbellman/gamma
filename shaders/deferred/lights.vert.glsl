614448ab0d2e9be7261726410d3eeda0f6e20686
unknown <mbellman@nwe.com>
2021-01-17 23:19:36 -0600

    Correcting/renaming inputs in the light disc vertex shader

--- shaders/deferred/lights.vert.glsl
+++ shaders/deferred/lights.vert.glsl
@@ -8,15 +8,14 @@ struct Light {
 };
 
 layout (location = 0) vec2 vertexPosition;
-layout (location = 1) vec2 scale;
-layout (location = 2) vec2 offset;
-layout (location = 3) Light currentLight;
+layout (location = 1) vec2 discOffset;
+layout (location = 2) vec2 discScale;
+layout (location = 3) Light discLight;
 
-noperspective out vec2 fragUv;
 flat out Light light;
 
 void main() {
-  gl_Position = vec4(vertexPosition * scale + offset, 0.0, 1.0);
+  gl_Position = vec4(vertexPosition * discScale + discOffset, 0.0, 1.0);
 
-  light = currentLight;
+  light = discLight;
 }
